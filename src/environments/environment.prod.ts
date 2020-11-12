8e0579cecba77dff2526de521fbf31a9deeee242
Kyle Cornwall <ckyle25@gmail.com>
2020-11-11 19:53:41 -0800

    updated prod server url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  serverUrl: 'https://cornwallia-api.azurewebsites.net/',
+  serverUrl: 'http://givingtree.cornwallia.com/',
   authDomain: 'ckylegaming.auth0.com',
   authClientId: '0i7iThu4rCmjdJK8RWjd9T4rDw2y7zRK'
 };
